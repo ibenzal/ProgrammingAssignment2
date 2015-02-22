a1c2a427ff9bfd01e782cfb68d2b77e23370e936
iban.benzal@gmail.com <iban.benzal@gmail.com>
2015-02-22 20:46:18 +0000

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverted <- NULL
+        set <- function(y) {
+                x <<- y
+                inverted <<- NULL
+        }
+        get <- function() x
+        setInverted <- function(newInverted) inverted <<- newInverted
+        getInverted <- function() inverted
+        list(set = set, get = get,
+             setInverted = setInverted,
+             getInverted = getInverted)
 }
 
 
@@ -12,4 +22,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverted <- x$getInverted()
+        if(!is.null(inverted)) {
+                message("getting cached data")
+                return(inverted)
+        }
+        data <- x$get()
+        inverted <- solve(data, ...)
+        x$setInverted(inverted)
+        inverted
 }
+
+inverted <-x$getInverted()
+if(!is.null(inverted))
+{
+        return(inverted)
+}
+data <- x$get()
+inverted<-solve(data)
+x$setInverted(inverted)
+inverted
